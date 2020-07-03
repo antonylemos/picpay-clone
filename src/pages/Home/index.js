723b1e2e84b4f472ee5fa7aa883d84237834c086
Antony Lemos <lemosantony@gmail.com>
2020-07-03 18:35:28 -0300

    💰 create activities components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,6 +11,7 @@ import {
 } from './styles';
 
 import Suggestions from '../../components/Suggestions';
+import Activities from '../../components/Activities';
 
 const Home = () => {
   return (
@@ -28,6 +29,8 @@ const Home = () => {
         </Header>
 
         <Suggestions />
+
+        <Activities />
       </Container>
     </Wrapper>
   );
